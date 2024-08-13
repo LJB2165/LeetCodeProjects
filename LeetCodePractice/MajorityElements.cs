9d24d36ae3bbc5b2abe8121c64899367311d8880
Lucas Balbi <175043874+LJB2165@users.noreply.github.com>
2024-08-13 16:00:24 -0700

    MAjorityElement

--- LeetCodePractice/MajorityElements.cs
+++ LeetCodePractice/MajorityElements.cs
@@ -8,6 +8,29 @@ namespace LeetCodePractice
 {
     public class MajorityElements
     {
+        public int MajorityElement(int[] nums)
+        {
+            int candidate = nums[0];
+            int count = 1;
 
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] == candidate)
+                {
+                    count++;
+                }
+                else
+                {
+                    count--;
+                    if (count == 0)
+                    {
+                        candidate = nums[i];
+                        count = 1;
+                    }
+                }
+            }
+
+            return candidate;
+        }
     }
 }
