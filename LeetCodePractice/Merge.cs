cd8826661e3f4d064704023898196c4577c40412
Lucas Balbi <175043874+LJB2165@users.noreply.github.com>
2024-08-13 10:39:21 -0700

    Updated changes

--- LeetCodePractice/Merge.cs
+++ LeetCodePractice/Merge.cs
@@ -1,35 +1,49 @@
-﻿namespace LeetCodePractice
+﻿public class Solution
 {
-    public class Solution
+    public void Merge(int[] nums1, int m, int[] nums2, int n)
     {
-        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        int p1 = m - 1; // Last element in the non-zero part of nums1
+        int p2 = n - 1; // Last element in nums2
+        int p = m + n - 1; // Last position in nums1
+
+        while (p1 >= 0 && p2 >= 0)
         {
-            int p1 = m - 1; // Last element in the non-zero part of nums1
-            int p2 = n - 1; // Last element in nums2
-            int p = m + n - 1; // Last position in nums1
-
-            while (p1 >= 0 && p2 >= 0)
+            if (nums1[p1] > nums2[p2])
             {
-                if (nums1[p1] > nums2[p2])
-                {
-                    nums1[p] = nums1[p1];
-                    p1--;
-                }
-                else
-                {
-                    nums1[p] = nums2[p2];
-                    p2--;
-                }
-                p--;
+                nums1[p] = nums1[p1];
+                p1--;
             }
-
-            // If there are remaining elements in nums2, place them in nums1
-            while (p2 >= 0)
+            else
             {
                 nums1[p] = nums2[p2];
-                p--;
                 p2--;
             }
+            p--;
         }
+
+        // If there are remaining elements in nums2, place them in nums1
+        while (p2 >= 0)
+        {
+            nums1[p] = nums2[p2];
+            p--;
+            p2--;
+        }
+    }
+}
+
+// Example usage:
+public class Program
+{
+    public static void Main()
+    {
+        int[] nums1 = { 1, 2, 3, 0, 0, 0 };
+        int m = 3;
+        int[] nums2 = { 2, 5, 6 };
+        int n = 3;
+
+        Solution solution = new Solution();
+        solution.Merge(nums1, m, nums2, n);
+
+        Console.WriteLine(string.Join(", ", nums1)); // Output: 1, 2, 2, 3, 5, 6
     }
 }
---
+++ LeetCodePracticeTests1/SolutionTests.cs
@@ -0,0 +1,99 @@
+﻿
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MergeArrayTests
+{
+    [TestClass]
+    public class MergeTests
+    {
+        [TestMethod]
+        public void Merge_BothArraysHaveElements_ShouldMergeCorrectly()
+        {
+            // Arrange
+            int[] nums1 = { 1, 2, 3, 0, 0, 0 };
+            int m = 3;
+            int[] nums2 = { 2, 5, 6 };
+            int n = 3;
+            int[] expected = { 1, 2, 2, 3, 5, 6 };
+
+            // Act
+            var solution = new Solution();
+            solution.Merge(nums1, m, nums2, n);
+
+            // Assert
+            CollectionAssert.AreEqual(expected, nums1);
+        }
+
+        [TestMethod]
+        public void Merge_EmptyNums2_ShouldRemainUnchanged()
+        {
+            // Arrange
+            int[] nums1 = { 1, 2, 3 };
+            int m = 3;
+            int[] nums2 = { };
+            int n = 0;
+            int[] expected = { 1, 2, 3 };
+
+            // Act
+            var solution = new Solution();
+            solution.Merge(nums1, m, nums2, n);
+
+            // Assert
+            CollectionAssert.AreEqual(expected, nums1);
+        }
+
+        [TestMethod]
+        public void Merge_EmptyNums1_ShouldCopyNums2()
+        {
+            // Arrange
+            int[] nums1 = { 0, 0, 0 };
+            int m = 0;
+            int[] nums2 = { 2, 5, 6 };
+            int n = 3;
+            int[] expected = { 2, 5, 6 };
+
+            // Act
+            var solution = new Solution();
+            solution.Merge(nums1, m, nums2, n);
+
+            // Assert
+            CollectionAssert.AreEqual(expected, nums1);
+        }
+
+        [TestMethod]
+        public void Merge_Nums1HasLargerElements_ShouldMergeCorrectly()
+        {
+            // Arrange
+            int[] nums1 = { 4, 5, 6, 0, 0, 0 };
+            int m = 3;
+            int[] nums2 = { 1, 2, 3 };
+            int n = 3;
+            int[] expected = { 1, 2, 3, 4, 5, 6 };
+
+            // Act
+            var solution = new Solution();
+            solution.Merge(nums1, m, nums2, n);
+
+            // Assert
+            CollectionAssert.AreEqual(expected, nums1);
+        }
+
+        [TestMethod]
+        public void Merge_Nums2HasLargerElements_ShouldMergeCorrectly()
+        {
+            // Arrange
+            int[] nums1 = { 1, 2, 3, 0, 0, 0 };
+            int m = 3;
+            int[] nums2 = { 4, 5, 6 };
+            int n = 3;
+            int[] expected = { 1, 2, 3, 4, 5, 6 };
+
+            // Act
+            var solution = new Solution();
+            solution.Merge(nums1, m, nums2, n);
+
+            // Assert
+            CollectionAssert.AreEqual(expected, nums1);
+        }
+    }
+}
