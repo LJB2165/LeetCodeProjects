f57c58eaea13657ebfdc022cf226cd09d3e7da68
Lucas Balbi <175043874+LJB2165@users.noreply.github.com>
2024-08-13 13:23:04 -0700

    RemoveElement

---
+++ LeetCodePracticeTests1/RemoveElementTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodePractice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice.Tests
+{
+    [TestClass()]
+    public class RemoveElementTests
+    {
+        [TestMethod()]
+        public void RemoveElementTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
