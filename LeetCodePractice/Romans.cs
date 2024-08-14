5a38aa77797dbfab88971751deb0f80d7bb44cad
Lucas Balbi <175043874+LJB2165@users.noreply.github.com>
2024-08-14 10:48:27 -0700

    Romans

---
+++ LeetCodePractice/Romans.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice
+{
+    public class RomanToIntegerConverter
+    {
+        public int RomanToInt(string s)
+        {
+            // Dictionary to store the value of each Roman numeral symbol
+            Dictionary<char, int> romanValues = new Dictionary<char, int>()
+        {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+
+            int total = 0; // Initialize the total to 0
+            int length = s.Length;
+
+            // Iterate through each character in the string
+            for (int i = 0; i < length; i++)
+            {
+                // Get the value of the current Roman numeral
+                int value = romanValues[s[i]];
+
+                // Check if we are not at the last character and the next character is larger
+                if (i + 1 < length && romanValues[s[i + 1]] > value)
+                {
+                    // If next character is larger, subtract the current value
+                    total -= value;
+                }
+                else
+                {
+                    // Otherwise, add the current value to the total
+                    total += value;
+                }
+            }
+
+            return total; // Return the computed integer value
+        }
+    }
+
+}
---
+++ LeetCodePracticeTests1/RomanToIntegerConverterTests.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodePractice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanToIntegerConverterTests
+{
+    [TestClass]
+    public class RomanToIntegerTests
+    {
+        [TestMethod]
+        public void TestRomanToInt_BasicCases()
+        {
+            // Arrange
+            RomanToIntegerConverter converter = new RomanToIntegerConverter();
+
+            // Act & Assert
+            Assert.AreEqual(3, converter.RomanToInt("III"));   // Simple addition case
+            Assert.AreEqual(4, converter.RomanToInt("IV"));    // Subtraction case
+            Assert.AreEqual(9, converter.RomanToInt("IX"));    // Subtraction case
+            Assert.AreEqual(58, converter.RomanToInt("LVIII")); // Mixed addition and subtraction
+            Assert.AreEqual(1994, converter.RomanToInt("MCMXCIV")); // Complex case with multiple subtractions
+        }
+
+        [TestMethod]
+        public void TestRomanToInt_EmptyString()
+        {
+            // Arrange
+            RomanToIntegerConverter converter = new RomanToIntegerConverter();
+
+            // Act
+            int result = converter.RomanToInt("");
+
+            // Assert
+            Assert.AreEqual(0, result); // An empty string should return 0
+        }
+
+        [TestMethod]
+        public void TestRomanToInt_SingleCharacter()
+        {
+            // Arrange
+            RomanToIntegerConverter converter = new RomanToIntegerConverter();
+
+            // Act & Assert
+            Assert.AreEqual(1, converter.RomanToInt("I"));
+            Assert.AreEqual(5, converter.RomanToInt("V"));
+            Assert.AreEqual(10, converter.RomanToInt("X"));
+            Assert.AreEqual(50, converter.RomanToInt("L"));
+            Assert.AreEqual(100, converter.RomanToInt("C"));
+            Assert.AreEqual(500, converter.RomanToInt("D"));
+            Assert.AreEqual(1000, converter.RomanToInt("M"));
+        }
+
+        [TestMethod]
+        public void TestRomanToInt_MaximumValue()
+        {
+            // Arrange
+            RomanToIntegerConverter converter = new RomanToIntegerConverter();
+
+            // Act
+            int result = converter.RomanToInt("MMMCMXCIX");
+
+            // Assert
+            Assert.AreEqual(3999, result); // The maximum possible value in Roman numerals
+        }
+
+        [TestMethod]
+        public void TestRomanToInt_MinimumValue()
+        {
+            // Arrange
+            RomanToIntegerConverter converter = new RomanToIntegerConverter();
+
+            // Act
+            int result = converter.RomanToInt("I");
+
+            // Assert
+            Assert.AreEqual(1, result); // The minimum possible value in Roman numerals
+        }
+    }
+}
