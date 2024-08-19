5496927073bc66dc25c183bc56ee84e3ac13654e
Lucas Balbi <175043874+LJB2165@users.noreply.github.com>
2024-08-19 15:48:36 -0700

    more exercises

---
+++ LeetCodePractice/summaryRange.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class SolutionSummaryRange
+{
+    public IList<string> SummaryRanges(int[] nums)
+    {
+        var ranges = new List<string>();
+        if (nums.Length == 0)
+        {
+            return ranges;
+        }
+
+        int start = nums[0];
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            // If the current number is not consecutive
+            if (nums[i] != nums[i - 1] + 1)
+            {
+                // Add the range to the list
+                if (start == nums[i - 1])
+                {
+                    ranges.Add(start.ToString());
+                }
+                else
+                {
+                    ranges.Add($"{start}->{nums[i - 1]}");
+                }
+                start = nums[i]; // Start a new range
+            }
+        }
+
+        // Add the final range
+        if (start == nums[nums.Length - 1])
+        {
+            ranges.Add(start.ToString());
+        }
+        else
+        {
+            ranges.Add($"{start}->{nums[nums.Length - 1]}");
+        }
+
+        return ranges;
+    }
+}
---
+++ LeetCodePracticeTests1/SolutionSummaryRangeTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class SolutionSummaryRangeTests
+    {
+        [TestMethod()]
+        public void SummaryRangesTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
+
+namespace LeetCodePracticeTests1
+{
+    internal class SolutionSummaryRangeTests
+    {
+    }
+}
